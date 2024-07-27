abf8becf4fd282f369fadd4e4bb9ec0486d8a618
usha1116 <ushasahu1116@gmail.com>
2024-07-27 16:48:32 +0530

    add about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,10 +16,38 @@ const About = () => {
                 </div>
                 <div className="about-right">
                     <div className="about-para">
-                        <p></p>
-                        <p></p>
+                        <p>I have honed my skills in HTML , CSS , JavaScript , C , C++ , php , vb.NET.</p>
+                        <p> Throughout my career, I have had the Achived ReactJS , Git_GitHub , WebDevlopment. I thrive in environments that encourage creativity, foster collaboration, and embrace diversity.</p>
+                    </div>
+                    <div className="about-skills">
+
+                        <div className="about-skill">
+                            <p>HTML & CSS<hr style={{width:"50%"}}/></p>
+                        </div>
+
+                        <div className="about-skill">
+                            <p>JavaSript<hr style={{width:"50%"}}/></p>
+                        </div>
+
+                        <div className="about-skill">
+                            <p>PHP<hr style={{width:"60%"}}/></p>
+                        </div>
+
+                        <div className="about-skill">
+                            <p>ReactJs<hr style={{width:"50%"}}/></p>
+                        </div>
+
+                        <div className="about-skill">
+                            <p>C & C++<hr style={{width:"70%"}}/></p>
+                        </div>
+
                     </div>
                 </div>
+            </div>
+            <div className="about-achivments">
+            <div className="about-achivment">
+                
+            </div>
             </div>
         </div>
     )
